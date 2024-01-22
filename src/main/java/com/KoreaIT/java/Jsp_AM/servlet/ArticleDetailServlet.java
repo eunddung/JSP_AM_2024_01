f6adb3180c0eb16c2c15bc0da5acb78328451f42
eunddung <x72845221@gmail.com>
2024-01-22 16:32:25 +0900

    SecSql 도입(SQL 인젝션)

--- src/main/java/com/KoreaIT/java/Jsp_AM/servlet/ArticleDetailServlet.java
+++ src/main/java/com/KoreaIT/java/Jsp_AM/servlet/ArticleDetailServlet.java
@@ -7,14 +7,17 @@ import java.sql.SQLException;
 import java.util.Map;
 
 import com.KoreaIT.java.Jsp_AM.util.DBUtil;
+import com.KoreaIT.java.Jsp_AM.util.SecSql;
 
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+
 @WebServlet("/article/detail")
 public class ArticleDetailServlet extends HttpServlet {
+
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		response.setContentType("text/html;charset=UTF-8");
@@ -25,9 +28,11 @@ public class ArticleDetailServlet extends HttpServlet {
 			System.out.println("클래스가 없습니다.");
 			e.printStackTrace();
 		}
+
 		String url = "jdbc:mysql://127.0.0.1:3306/JSP_AM?useUnicode=true&characterEncoding=utf8&autoReconnect=true&serverTimezone=Asia/Seoul&useOldAliasMetadataBehavior=true&zeroDateTimeNehavior=convertToNull";
 		String user = "root";
 		String password = "";
+
 		Connection conn = null;
 
 		try {
@@ -36,12 +41,15 @@ public class ArticleDetailServlet extends HttpServlet {
 
 			int id = Integer.parseInt(request.getParameter("id"));
 
-			String sql = String.format("SELECT * FROM article WHERE id = %d;", id);
+			SecSql sql = SecSql.from("SELECT *");
+			sql.append("FROM article");
+			sql.append("WHERE id = ?;", id);
 
 			Map<String, Object> articleRow = DBUtil.selectRow(conn, sql);
 
 			request.setAttribute("articleRow", articleRow);
 			request.getRequestDispatcher("/jsp/article/detail.jsp").forward(request, response);
+
 		} catch (SQLException e) {
 			System.out.println("에러 : " + e);
 		} finally {
@@ -54,4 +62,5 @@ public class ArticleDetailServlet extends HttpServlet {
 			}
 		}
 	}
+
 }
